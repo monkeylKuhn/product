ad6114c6ef452a25a52452580055245a2e82e5ee
monkeyLKuhn <1615325074@qq.com>
2020-01-07 21:44:05 +0800

    定时任务提交

--- src/main/java/com/simple/productInfo/task/TestStockTask.java
+++ src/main/java/com/simple/productInfo/task/TestStockTask.java
@@ -1,8 +1,9 @@
 package com.simple.productInfo.task;
 
-import java.util.Date;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -10,9 +11,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
 
 import com.alibaba.fastjson.JSONObject;
-import com.simple.productInfo.mapper.DressProductMapper;
 import com.simple.productInfo.mapper.DressSkuMapper;
-import com.simple.productInfo.model.DressProduct;
 import com.simple.productInfo.model.DressSkuSize;
 import com.simple.productInfo.model.DressStock;
 import com.simple.productInfo.model.DressStockResult;
@@ -21,14 +20,11 @@ import com.simple.productInfo.model.DressStockResult;
 public class TestStockTask {
 	
 	@Autowired
-	DressProductMapper dressProductMapper;
-	
-	@Autowired
 	DressSkuMapper dressSkuMapper;
 
-	@Scheduled(cron = "0 0/20 * * * ?")
-	public void fetchProduct() {
-		int total = 0;
+//	@Scheduled(cron = "0 0/20 * * * ?")
+	@Scheduled(fixedDelay=1000*60*1)
+	public void fetchStock() {
 		String url = "https://api.dresscode.cloud/channels/v2/api/feeds/en/clients/llf/stocks?channelKey=0198873e-1fde-4783-8719-4f1d0790eb6e";
 		HashMap<String, String> head = new HashMap<String,String>();
 		head.put("Ocp-Apim-Subscription-Key", "107b04efec074c6f8f8abed90d224802");
@@ -44,7 +40,7 @@ public class TestStockTask {
 			// 1 更新中  2库存更新 3价格更新 4均有更新 5未更新且库存不为0  6新插入数据
 			// 全部设置状态为 1 更新中
 			// 查询当前表是否存在 productID+size
-			// 如果存在 比较库存及价格是否一致
+			// 如果存在 比较库存及价格是否一致 
 			// 一致 -- 不进行操作
 			// 不一致 -- 更新并设置不同更新状态
 			// 如果不存在，插入，并设置更新状态为 4库存价格均更新
@@ -85,16 +81,23 @@ public class TestStockTask {
                 }
             }
 			
-			
-			
 			// 查询有库存更新的   2  3  4
+			Set<Integer> statusList = new HashSet<>();
+			statusList.add(2);statusList.add(3);statusList.add(4);
+			List<DressSkuSize> list = dressSkuMapper.selectByStatusAndStock(null, statusList);
 			
 			// 查询新插入的  6
+			Set<Integer> statusList2 = new HashSet<>();
+			statusList2.add(6);
+			List<DressSkuSize> list2 = dressSkuMapper.selectByStatusAndStock(null, statusList2);
 			
 			// 查询库存要更新为0的    1 and stock>0
+			Set<Integer> statusList3 = new HashSet<>();
+			statusList3.add(1);
+			List<DressSkuSize> list3 = dressSkuMapper.selectByStatusAndStock(null, statusList3);
 			
 			// 更新库存为0
-			
+			dressSkuMapper.updateStatusByStock(1);
 			
 		} catch (Exception e) {
 			e.printStackTrace();
